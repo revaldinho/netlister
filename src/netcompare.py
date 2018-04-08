7c885b9f0f4a4ee43db07023249241d361a6e61d
revaldinho <richevans@mac.com>
2018-04-08 13:38:22 +0100

    Minor updates to be python2.7/Python3+ compatible in print()/iterkeys()/xrange() fns

--- src/netcompare.py
+++ src/netcompare.py
@@ -32,7 +32,7 @@ def read_eagle_file ( filename ) :
                 if not connection_list :
                     key = words[0]
                     words = words[1:]
-                for i in xrange ( 0, len(words), 2 ) :                    
+                for i in range ( 0, len(words), 2 ) :                    
                     if words[i] in connection_list :
                         connection_list[words[i]].append( words[i+1])
                     else:
@@ -46,11 +46,11 @@ def read_eagle_file ( filename ) :
 def compare_netlists ( net1, net2 ) :
     # Decidedly non-optimal comparison of the two netlists
     match = True
-    for k in net1.iterkeys() :
+    for k in net1.keys() :
         # Check all nets in netlist1 are found in netlist2
         if not k in net2 :
             match = False
-            print "Net %s in file1 but not in file2" % k
+            print ("Net %s in file1 but not in file2" % k)
         else : 
             # For nets in both netlists check first
             # that the components the nets are connected to
@@ -61,35 +61,35 @@ def compare_netlists ( net1, net2 ) :
             for c in n1_components:
                 if c not in n2_components:
                     match = False
-                    print "Fail on net %s : has connection to component %s in file1 but not in file2" %( k,c)
+                    print ("Fail on net %s : has connection to component %s in file1 but not in file2" %( k,c))
                 else:
                     if sorted((net1[k])[c]) != sorted((net2[k])[c]):
                         match = False
-                        print "Mismatch on connections for component", c
-                        print "file1:", (net1[k])[c]
-                        print "file2:", (net2[k])[c]
+                        print ("Mismatch on connections for component", c)
+                        print ("file1:", (net1[k])[c])
+                        print ("file2:", (net2[k])[c])
                         
             for c in n2_components:
                 if c not in n1_components:
                     match = False
-                    print "Fail on net %s : has connection to component %s in file2 but not in file1" %( k,c)
+                    print( "Fail on net %s : has connection to component %s in file2 but not in file1" %( k,c))
                     
 
-    for k in net2.iterkeys() :
+    for k in net2.keys() :
         # Check all nets in netlist2 are found in netlist1
         if not k in net1 :
             match = False
-            print "Net %s in file2 but not in file1" % k
+            print( "Net %s in file2 but not in file1" % k)
             
     if match : 
-        print "PASS - netlists match"
+        print ("PASS - netlists match")
     else :
-        print "FAIL - netlists don't match"
+        print ("FAIL - netlists don't match")
 
 
 if __name__ == "__main__" :
     if len(sys.argv) < 3 :
-        print "Usage: netcmp.py file1 file2"
+        print ("Usage: netcmp.py file1 file2")
         sys.exit()
                 
     net1 = read_eagle_file( sys.argv[1] )
